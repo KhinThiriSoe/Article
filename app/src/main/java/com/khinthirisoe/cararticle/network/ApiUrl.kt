a5eda0cccfa73282d3ab36ec068eee77a27bafde
Khin <khinthirisoe.mdy@gmail.com>
2019-08-04 13:06:54 +0700

    network service

---
+++ app/src/main/java/com/khinthirisoe/cararticle/network/ApiUrl.kt
@@ -0,0 +1,6 @@
+package com.khinthirisoe.cararticle.network
+
+const val BASE_URL = "https://www.apphusetreach.no/application/119267/"
+
+/* Get all articles */
+const val GET_ARTICLE_LIST = "article/get_articles_list"
---
+++ app/src/main/java/com/khinthirisoe/cararticle/network/Service.kt
@@ -0,0 +1,34 @@
+package com.khinthirisoe.cararticle.network
+
+import com.google.gson.Gson
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import kotlinx.coroutines.Deferred
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+interface ArticleService {
+    @GET(GET_ARTICLE_LIST)
+    fun getArticleList(): Deferred<Article>
+}
+
+object Network {
+
+    private val okHttpClient = OkHttpClient().newBuilder()
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        })
+        .build()
+
+    // Configure retrofit to parse JSON and use coroutines
+    private val retrofit = Retrofit.Builder()
+        .client(okHttpClient)
+        .baseUrl(BASE_URL)
+        .addCallAdapterFactory(CoroutineCallAdapterFactory())
+        .addConverterFactory(GsonConverterFactory.create(Gson()))
+        .build()
+
+    val articleService = retrofit.create(ArticleService::class.java)
+}
