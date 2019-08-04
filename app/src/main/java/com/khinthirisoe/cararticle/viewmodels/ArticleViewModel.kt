cb6474bb55b2651468e12b8db1eea6541a003574
Khin <khinthirisoe.mdy@gmail.com>
2019-08-04 14:26:48 +0700

    binding adapter

--- app/src/main/java/com/khinthirisoe/cararticle/viewmodels/ArticleViewModel.kt
+++ app/src/main/java/com/khinthirisoe/cararticle/viewmodels/ArticleViewModel.kt
@@ -1,1 +1,46 @@
 package com.khinthirisoe.cararticle.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.khinthirisoe.cararticle.database.getDatabase
+import com.khinthirisoe.cararticle.repository.ArticleRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+
+class ArticleViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val viewModelJob = SupervisorJob()
+
+    private val viewModelScope = CoroutineScope(viewModelJob + Dispatchers.Main)
+
+    private val database = getDatabase(application)
+
+    private val articleRepository = ArticleRepository(database)
+
+    init {
+        viewModelScope.launch {
+            articleRepository.refreshArticles()
+        }
+    }
+
+    val articles = articleRepository.articles
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+
+    class Factory(val app: Application) : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(ArticleViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return ArticleViewModel(app) as T
+            }
+            throw IllegalArgumentException("Unable to construct viewmodel")
+        }
+    }
+}
