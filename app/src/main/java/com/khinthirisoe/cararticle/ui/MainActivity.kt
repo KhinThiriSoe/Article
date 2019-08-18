be51e36e85e43d0f578f382731a1011ef6ffd058
Khin <khinthirisoe.mdy@gmail.com>
2019-08-18 18:07:38 +0700

    add navigationup

--- app/src/main/java/com/khinthirisoe/cararticle/ui/MainActivity.kt
+++ app/src/main/java/com/khinthirisoe/cararticle/ui/MainActivity.kt
@@ -2,6 +2,8 @@ package com.khinthirisoe.cararticle.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.khinthirisoe.cararticle.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +11,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.my_nav_host_fragment)
+        setupActionBarWithNavController(navController)
     }
+
+    override fun onSupportNavigateUp() =
+        findNavController(R.id.my_nav_host_fragment).navigateUp()
 }
